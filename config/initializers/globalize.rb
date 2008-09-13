3d14af707c9fb342dc9ef0d6e742064363d71ef6
Sven Fuchs <svenfuchs@artweb-design.de>
2008-09-13 20:55:02 +0200

    playing with Static backend and fallbacks

--- config/initializers/globalize.rb
+++ config/initializers/globalize.rb
@@ -1,5 +1,8 @@
 ActiveRecord::Base.send :include, Globalize::Model::ActiveRecord::Translated
 I18n.send :include, Globalize::LoadPath::I18n
 
+# require 'globalize/backend/chain'
+# I18n.chain_backends "Globalize::Backend::Static", I18n.backend
+
 I18n.load_path << "#{RAILS_ROOT}/lib/locale"
-I18n.load_locales 'en-US', 'de-DE'
+I18n.load_locales 'en', 'de'
