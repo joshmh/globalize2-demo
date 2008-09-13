f03d388a170e01e85f611caf80283b9f7c0e4564
Sven Fuchs <svenfuchs@artweb-design.de>
2008-09-13 21:31:54 +0200

    clean up globalize initializer

--- config/initializers/globalize.rb
+++ config/initializers/globalize.rb
@@ -1,8 +1,6 @@
 ActiveRecord::Base.send :include, Globalize::Model::ActiveRecord::Translated
 I18n.send :include, Globalize::LoadPath::I18n
 
-# require 'globalize/backend/chain'
-# I18n.chain_backends "Globalize::Backend::Static", I18n.backend
-
 I18n.load_path << "#{RAILS_ROOT}/lib/locale"
-I18n.load_locales 'en', 'de'
+I18n.load_locales :en, :de
+I18n.fallbacks.map :de => :en, :he => :en
