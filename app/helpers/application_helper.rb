d88ba98bf4d4383d540357264efc51cb201881a9
Josh Harvey <joshmh@gmail.com>
2008-09-18 00:31:26 +0300

    added right-to-left; hebrew translations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def text_direction
+    I18n.translate(:'bidi.direction') == 'right-to-left' ? 'rtl' : 'ltr'
+  end
 end
